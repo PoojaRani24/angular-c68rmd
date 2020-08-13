002086f90251878f529e333e8c77d8b4d5bad16c
PoojaRani24 <pooja.16bit1091@abes.ac.in>
2020-08-13 19:26:57 +0530

    3 files modified, 3 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,10 +1,14 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'; 
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn:'root'
 })
 export class CartService {
   items=[];
+  constructor(
+    private http:HttpClient
+  ){}
   addToCart(product){
     this.items.push(product);
   }
@@ -15,4 +19,8 @@ export class CartService {
     this.items=[];
     return this.items;
   }
+  getShippingPrices(){
+    return this.http.get('/assets/shipping.json')
+  }
+
 }
